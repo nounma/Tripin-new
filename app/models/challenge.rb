bc30e8390660baddf137336eb148adf1a51438b8
Alissa De Wannemaeker <AlissaDeWan@gmail.com>
2016-12-05 18:02:36 +0100

    has_many belongs_to done

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,7 @@
 class Challenge < ApplicationRecord
+
+  belongs_to :city
+  has_many :teams, through: :team_challenges
+  has_many :answers
+
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,9 @@
 class City < ApplicationRecord
+
+  CITIES = %w ( Brussels Paris Amsterdam London )
+  
+  has_many :challenges
+  
+  validates :name, presence: true, inclusion: { in: CITIES }
+
 end
