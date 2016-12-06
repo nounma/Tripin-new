81df7173444e1be1c0da5968502b7f63810fc85e
My Kim Bui <mykimbuibui@gmail.com>
2016-12-06 11:57:04 +0100

    Added show controller but not correct yet

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,34 +1,28 @@
 class ChallengesController < ApplicationController
   def index
-
     @user = User.find(params[:user_id])
-
-selectionner les challenges city = city choisie
-
-@team = Team.find(team_params[:team_id])
-trip_length = end_date - start_date (model teams)
-
-if trip_length > 3
-  num_challenge = 20
-else trip_length < 3
-  num_challenge = 50
-end
-
-
-  end
+    @team = Team.find(team_params[:team_id])
 
   def show
+    # @user = User.find(params[:user_id])
+    # @team = Team.find(team_params[:team_id])
+    @challenge = Challenge.find(params[:id])
+
+
+
   end
 
-private
-  def set_profile
-    @user = User.find(params[:id])
-  end
+  private
 
- def user_params
-    params.require(:user).permit(:id)
-  end
-def team_params
+    def set_profile
+      @user = User.find(params[:id])
+    end
 
-    params.require(:user).permit(:start_date, :end_date)
-  end
+    def user_params
+      params.require(:user).permit(:id)
+    end
+
+    def team_params
+
+      params.require(:user).permit(:start_date, :end_date)
+    end
--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,7 +1,11 @@
 class Challenge < ApplicationRecord
 
+  INPUT_TYPE = %w(picture text both)
+
   belongs_to :city
   has_many :teams, through: :team_challenges
   has_many :answers
 
+  validates :input_type, presence: true, inclusion: { in: INPUT_TYPE }
+  validates :good_answer, presence: true
 end
