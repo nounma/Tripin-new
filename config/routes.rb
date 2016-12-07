af9a1469e08c446b19d0b6d96242aa826ff7933d
Manon Champredon <mah-non@hotmail.com>
2016-12-07 11:53:18 +0100

    routes change + user controller + log in fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:show, :edit, :update]
+    resources :teams, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+      resources :challenges, only: [:index, :show] do
+        resources :answers
 
-  resources :teams, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :challenges, only: [:index, :show] do
-      resources :answers
-
+      end
+      post '/add_member', to: 'teams#add_member'
     end
-    post '/add_member', to: 'teams#add_member'
-  end
-  resources :cities, only: [:index, :show]
+    resources :cities, only: [:index, :show]
+
 
 end
