43e37042a6bea5fb7fbadf93834eb1c63d43df0b
sophie de coninck <scourbey@gmail.com>
2016-12-07 12:01:03 +0100

    log avec facebook

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'carrierwave'
 gem "geocoder"
 gem "coffee-rails"
 gem "gmaps4rails"
+gem 'omniauth-facebook'
 
 source 'https://rails-assets.org' do
   gem "rails-assets-underscore"
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get 'members/new'
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
